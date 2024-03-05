04935e11eb3c47e54515e90f7596cfca1883312d
Sami Kalammallah <skalexsong@gmail.com>
2024-03-05 23:52:13 +0700

    chore: add configuration
    Co-authored-by: Donni Husnan <donnihusnan@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     remotePatterns: [
       {
         hostname: "media.suara.com",
+        protocol: "https",
+      },
+      {
+        hostname: "image.tmdb.org",
         protocol: "https",
       },
     ],
